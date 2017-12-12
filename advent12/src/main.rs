e014bb6489df8f405c7c1ab67d0487294908425e
pete gamache <pete@gamache.org>
2017-12-12 08:13:31 -0500

    day 12 part 2

--- advent12/src/main.rs
+++ advent12/src/main.rs
@@ -26,13 +26,30 @@ fn main() {
   }
 }
 
-fn answers(input: &str) -> (usize, i32) {
+fn answers(input: &str) -> (usize, usize) {
   let connections_map = parse_input(input);
-  let part1 = get_programs_connected_to(connections_map, 0).len();
-  return (part1, 0);
+
+  let part1 = get_programs_connected_to(&connections_map, 0).len();
+
+  let groups: Vec<Vec<i32>> = connections_map
+    .keys()
+    .map(|p| get_programs_connected_to(&connections_map, *p))
+    .collect();
+  let mut groups_set = HashSet::new();
+  for group in groups { groups_set.insert(group_to_string(group)); }
+  let part2 = groups_set.len();
+
+  return (part1, part2);
 }
 
-fn get_programs_connected_to(map: HashMap<i32, Vec<i32>>, id: i32) -> Vec<i32> {
+fn group_to_string(group: Vec<i32>) -> String {
+  let mut g = group.clone();
+  g.sort();
+  let g_strs: Vec<String> = g.iter().map(|x| x.to_string()).collect();
+  return g_strs.join("-");
+}
+
+fn get_programs_connected_to(map: &HashMap<i32, HashSet<i32>>, id: i32) -> Vec<i32> {
   let mut programs: HashSet<i32> = HashSet::new();
   programs.insert(id);
 
@@ -55,7 +72,7 @@ fn get_programs_connected_to(map: HashMap<i32, Vec<i32>>, id: i32) -> Vec<i32> {
   return programs.iter().cloned().collect();
 }
 
-fn parse_input(input: &str) -> HashMap<i32, Vec<i32>> {
+fn parse_input(input: &str) -> HashMap<i32, HashSet<i32>> {
   let mut connections = HashMap::new();
 
   let line_conns: Vec<Connection> = input
@@ -69,7 +86,7 @@ fn parse_input(input: &str) -> HashMap<i32, Vec<i32>> {
 
 struct Connection {
   from: i32,
-  to: Vec<i32>,
+  to: HashSet<i32>,
 }
 
 impl Connection {
@@ -85,10 +102,12 @@ impl Connection {
     let caps = LINE_REGEX.captures(line)?;
 
     let from = caps.get(1)?.as_str().parse::<i32>().ok()?;
-    let to = caps.get(2)?.as_str()
+    let to_vec: Vec<i32> = caps.get(2)?.as_str()
       .split(", ")
       .flat_map(|n| n.parse::<i32>())
       .collect();
+    let mut to = HashSet::new();
+    for x in to_vec { to.insert(x); }
     return Some(Connection { from, to });
   }
 }
