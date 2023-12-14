347e1dba42dc50b064b112e926d812f8bb0cba1a
pete gamache <pete@gamache.org>
2023-12-14 17:53:46 -0500

    checkpoint

--- day12.go
+++ day12.go
@@ -9,12 +9,11 @@ import (
 
 func Day12() {
 	lines := GetLines("inputs/day12.txt")
+	// lines := GetLines("inputs/example12.txt")
 
 	sum := 0
 	for _, line := range lines {
-		fmt.Println(line)
 		parts := strings.Split(line, " ")
-
 		v := countValidArrangements(parts[0], parts[1])
 		sum += v
 	}
@@ -23,37 +22,57 @@ func Day12() {
 	// part 2
 
 	wg := sync.WaitGroup{}
-	work := make(chan string)
-	results := make(chan int)
-
-	for i := 0; i < 64; i++ {
+	n := 512
+	work := make(chan string, n)
+	results := make(chan int, n)
+	for i := 0; i < n; i++ {
 		go func() {
 			wg.Add(1)
 			defer wg.Done()
-
 			for {
 				line, ok := <-work
 				if !ok {
 					break
 				}
-				fmt.Println(line)
 				parts := strings.Split(line, " ")
-				results <- countValidArrangements(
-					strings.Join([]string{
-						parts[0],
-						parts[0],
-						parts[0],
-						parts[0],
-						parts[0],
-					}, "?"),
-					strings.Join([]string{
-						parts[1],
-						parts[1],
-						parts[1],
-						parts[1],
-						parts[1],
-					}, ","),
+
+				v := countValidArrangements(parts[0], parts[1])
+				v2 := countValidArrangements(
+					parts[0]+"?"+parts[0],
+					parts[1]+","+parts[1],
 				)
+				joinRatio := float32(v2) / float32(v)
+
+				if joinRatio == float32(int32(joinRatio)) {
+					// Easy way
+					r := int(joinRatio)
+					r = v * r * r * r * r
+					results <- r
+					continue
+				}
+
+				v3 := countValidArrangements(
+					parts[0]+"?"+parts[0]+"?"+parts[0],
+					parts[1]+","+parts[1]+","+parts[1],
+				)
+				joinRatio32 := float32(v3) / float32(v2)
+
+				v4 := countValidArrangements(
+					parts[0]+"?"+parts[0]+"?"+parts[0]+"?"+parts[0],
+					parts[1]+","+parts[1]+","+parts[1]+","+parts[1],
+				)
+				joinRatio43 := float32(v4) / float32(v3)
+				v5 := countValidArrangements(
+					parts[0]+"?"+parts[0]+"?"+parts[0]+"?"+parts[0]+"?"+parts[0],
+					parts[1]+","+parts[1]+","+parts[1]+","+parts[1]+","+parts[1],
+				)
+				joinRatio54 := float32(v5) / float32(v4)
+
+				fmt.Println(line)
+				fmt.Printf("%v %v %v %v %v\n", v, v2, v3, v4, v5)
+				fmt.Printf("%v %v %v %v\n\n", joinRatio, joinRatio32, joinRatio43, joinRatio54)
+
+				results <- v5
 			}
 		}()
 	}
@@ -67,11 +86,15 @@ func Day12() {
 		close(work)
 	}()
 
-	sum2 := 0
-	for _ = range lines {
-		sum2 += <-results
+	sum = 0
+	for i := range lines {
+		fmt.Println(i)
+		sum += <-results
 	}
-	fmt.Println(sum2)
+
+	// 630375483449 is too low
+	// 888491519337 is too high
+	fmt.Println(sum)
 
 	wg.Wait()
 }
@@ -86,58 +109,14 @@ func countValidArrangements(springMapStr, patternStr string) int {
 		pattern[i] = p
 	}
 
-	arrangements := cva(
+	return cva(
 		strings.Split(springMapStr+".", ""),
 		pattern,
 		0,
 	)
-
-	// fmt.Printf("%v %v %v\n", springMapStr, patternStr, arrangements)
-
-	return arrangements
-	/*
-	   // fmt.Println(springMapStr)
-	   count := 0
-
-
-
-	   arrangements := []string{springMapStr}
-
-	   	for _, str := range strings.Split(springMapStr, "") {
-	   		if str == "?" {
-	   			newArrangements := []string{}
-	   			for _, arr := range arrangements {
-	   				// fmt.Println(arr)
-	   				newArr1 := strings.Replace(arr, "?", ".", 1)
-	   				if followsPatternSoFar(strings.Split(newArr1, ""), pattern) {
-	   					newArrangements = append(newArrangements, newArr1)
-	   				}
-	   				newArr2 := strings.Replace(arr, "?", "#", 1)
-	   				if followsPatternSoFar(strings.Split(newArr2, ""), pattern) {
-	   					newArrangements = append(newArrangements, newArr2)
-	   				}
-	   			}
-	   			arrangements = newArrangements
-	   		}
-	   	}
-
-	   // fmt.Println(arrangements)
-
-	   	for _, arr := range arrangements {
-	   		springMap := strings.Split(arr, "")
-	   		springMap = append(springMap, ".") // makes logic easier below
-	   		if followsPattern(springMap, pattern) {
-	   			count++
-	   		}
-	   	}
-
-	   return count
-	*/
 }
 
 func cva(springMap []string, pattern []int, startAt int) int {
-	// fmt.Printf("cva   %v %v %v\n", springMap, pattern, startAt)
-
 	for i := startAt; i < len(springMap); i++ {
 		if springMap[i] == "?" {
 			sm1 := make([]string, len(springMap))
@@ -161,7 +140,6 @@ func cva(springMap []string, pattern []int, startAt int) int {
 	}
 
 	if followsPattern(springMap, pattern) {
-		// fmt.Printf("match %v %v\n", springMap, pattern)
 		return 1
 	} else {
 		return 0
@@ -169,8 +147,6 @@ func cva(springMap []string, pattern []int, startAt int) int {
 }
 
 func followsPattern(springMap []string, pattern []int) bool {
-	// fmt.Printf("followsPattern %v %v\n", springMap, pattern)
-
 	count := 0
 	last := "."
 
@@ -197,8 +173,6 @@ func followsPattern(springMap []string, pattern []int) bool {
 }
 
 func followsPatternSoFar(springMap []string, pattern []int) bool {
-	// fmt.Printf("followsPatternSoFar %v %v\n", springMap, pattern)
-
 	count := 0
 	last := "."
 
